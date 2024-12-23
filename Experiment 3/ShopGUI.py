bd950cbd42df19a3c1c9cc5d54f3733bc76a9f42
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-12-22 22:39:43 -0600

    Successfully created a functional GUI with Tkinter!

---
+++ Experiment 3/ShopGUI.py
@@ -0,0 +1,12 @@
+from tkinter import * 
+
+# Instantiates an instance of a window 
+window = Tk()
+
+# Sets the size of the GUI
+window.geometry("420x420")
+
+# Sets the title of the GUI 
+window.title("Shop GUI")
+
+window.mainloop()
