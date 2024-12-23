912edac252c336fad66e706b49d975a2722447d9
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-12-22 22:55:29 -0600

    Successfuly displayed welcome message with design on the GUI!

--- Experiment 3/ShopGUI.py
+++ Experiment 3/ShopGUI.py
@@ -4,9 +4,23 @@ from tkinter import *
 window = Tk()
 
 # Sets the size of the GUI
-window.geometry("420x420")
+window.geometry("640x840")
 
 # Sets the title of the GUI 
 window.title("Shop GUI")
 
+# Sets the appearance of the GUI
+window.config(background="#8FDAFA")
+
+# Adds a weclome message to the GUI for display
+welcomeMessage = Label(window, 
+                       text="Welcome to the GUI!",
+                       font=('Arial', 40, 'bold'),
+                       fg='black',
+                       bg='#8FDAFA',
+                       padx=20,
+                       pady=40)
+welcomeMessage.pack()
+
+# Places GUI on computer screen that listen for events! 
 window.mainloop()
