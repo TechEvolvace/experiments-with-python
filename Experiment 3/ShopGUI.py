f335f6740b94d881af6ca738cb25f688055fc265
TechEvolvace <EvolvaceTechnical.PhucDinh@proton.me>
2024-12-29 23:52:09 -0600

    Installed Pillow to get resize methods from the Python Image Library's Image and ImageTk library!

--- Experiment 3/ShopGUI.py
+++ Experiment 3/ShopGUI.py
@@ -1,5 +1,5 @@
-from tkinter import * 
-from PIL import Image, ImageTk
+from tkinter import *  # Installed Tkinter by conda and pip!
+from PIL import Image, ImageTk  # Installed pillow by Conda!
 
 # Instantiates an instance of a window 
 window = Tk()
@@ -30,24 +30,33 @@ entry = Entry(window,
               fg='#21fc0d')
 entry.pack()
 
-# Add the Product images to the GUI!
+# Read the image files and get all of the product images!
 product_1_image = Image.open("./Assets/Shop-GUI-Product-1.gif")
 
 product_2_image = Image.open("./Assets/Shop-GUI-Product-2.gif")
 
 product_3_image = Image.open("./Assets/Shop-GUI-Product-3.gif")
 
+# Resized the product images!
 resized_product_1_image = product_1_image.resize((250, 250))
 
 resized_product_2_image = product_2_image.resize((250, 250))
 
 resized_product_3_image = product_3_image.resize((250, 250))
 
+# Reformatted each of the resized images with PhotoImage function!
+reformatted_product_1_image = ImageTk.PhotoImage(resized_product_1_image)
+
+reformatted_product_2_image = ImageTk.PhotoImage(resized_product_2_image)
+
+reformatted_product_3_image = ImageTk.PhotoImage(resized_product_3_image)
+
+# Displaying each product image in the GUI!
 product_image_gallery = Canvas(window, width=640, height=600)
 product_image_gallery.pack()
-product_image_gallery.create_image(0, 20, image = product_1_image)
-product_image_gallery.create_image(0, 90, image = product_2_image)
-product_image_gallery.create_image(0, 140, image = product_3_image)
+product_image_gallery.create_image(0, 20, image = reformatted_product_1_image)
+product_image_gallery.create_image(0, 90, image = reformatted_product_2_image)
+product_image_gallery.create_image(0, 140, image = reformatted_product_3_image)
 
 
 # Places GUI on computer screen that listen for events! 
